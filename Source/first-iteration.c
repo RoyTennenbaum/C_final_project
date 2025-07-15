68d92301d8115874a4b451f74c1317f857769bbc
RoyTennenbaum <roytenen10@gmail.com>
2025-07-16 00:46:26 +0300

    wip: first-iteration

--- Source/first-iteration.c
+++ Source/first-iteration.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
-#include "first-iteration.h"
+#include <ctype.h>
+#include <string.h>
+#include "../Headers/static-tables.h"
 
 int firstIteration(char *fileName) {
     int IC = 0, DC = 0;
 
-    char *lineStr, *firstStr;
+    char *line, *arg;
     int lineNum = 0; /* Mark line number for future error messages */
-    File *fp = fopen(fileName, "r");
-    int scan; /* Scan status */
+    FILE *fp = fopen(fileName, "r");
 
-    if (fp != NULL) {
+    if (fp == NULL) {
         printf("Error: file could not be opened.");
         return 1;
     }
 
-    while (fgets(lineStr, LINE_SIZE, fp) != NULL) {
+    while (fgets(line, LINE_SIZE, fp) != NULL) {
         lineNum++;
 
-        scan = sscanf(lineStr, "%s ", firstStr);
-        if (scan != SCAN_OK) {
-            printf("Error: scan type mismatch.");
-            return 1;
-        }
+        arg = strtok(line, " ");
 
-        /* if(isSymbol(symbolTable, firstStr)) */
-        /* isOperation(operationTable, firstStr, opNum) */
-        /* isDirective(directiveTable, firstStr) */
+        if (isValidSymbol(arg)) {
+            symbolFlag = 1;
+        }
     }
 
     fclose(fp);
     return 0;
+}
+
+int isValidSymbol(char *str) {
+    site_t len = strlen(str);
+    if (len > 30 || !isAlpha(str[0]) || strcmp(str[len - 1], ':') != 0 ||
+        isOperation(str) || isDirective(str) || isSymbol(str) || isMacro(str)) {
+        return 0;
+    }
+    return 1;
 }
