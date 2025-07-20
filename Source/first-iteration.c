2c160b51df981a257b727fff0bcda69d6db5250c
RoyTennenbaum <roytenen10@gmail.com>
2025-07-21 00:45:50 +0300

    wip: first-iteration

--- Source/first-iteration.c
+++ Source/first-iteration.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include "../Headers/static-tables.h"
 
-int firstIteration(char *fileName) {
-    int IC = 0, DC = 0;
+int firstIteration(char *fileName, context) {
+    int IC = 100, DC = 0;
 
     char *line, *arg;
     int lineNum = 0; /* Mark line number for future error messages */
     FILE *fp = fopen(fileName, "r");
+
+    int symbolFlag = 0;
 
     if (fp == NULL) {
         printf("Error: file could not be opened.");
@@ -20,8 +22,13 @@ int firstIteration(char *fileName) {
 
         arg = strtok(line, " ");
 
-        if (isValidSymbol(arg)) {
+        if (isNewSymbol(arg)) {
             symbolFlag = 1;
+        }
+
+        if (isDirective(arg)) {
+            insert(context.symbolTable, arg, DC);
+            /* if not directive, go to 8 in algo */
         }
     }
 
@@ -29,8 +36,12 @@ int firstIteration(char *fileName) {
     return 0;
 }
 
-int isValidSymbol(char *str) {
+int isNewSymbol(char *str) {
     site_t len = strlen(str);
+    /* failsafe to ensure len-1 will always be valid */
+    if (len < 1) {
+        return 0;
+    }
     if (len > 30 || !isAlpha(str[0]) || strcmp(str[len - 1], ':') != 0 ||
         isOperation(str) || isDirective(str) || isSymbol(str) || isMacro(str)) {
         return 0;
