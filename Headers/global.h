0a2d9610b706fda1b45e1450936b54889f7f8c78
RoyTennenbaum <roytenen10@gmail.com>
2025-07-23 15:48:54 +0300

    global definitions

--- Headers/global.h
+++ Headers/global.h
@@ -1,9 +1,9 @@
 #include "dynamic-tables.h"
 #include "static-tables.h"
 
-enum {FALSE, TRUE};
-
-enum {SUCCESS, ERROR};
+enum { FALSE, TRUE };
+enum { SUCCESS, ERROR };
+#define LINE_SIZE 83
 
 typedef struct {
     symbolTable *symbolTable;
