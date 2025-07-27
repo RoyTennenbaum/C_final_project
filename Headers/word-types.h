75f071e9e1b7d751c4453b3692e4dcf9e00a82e2
RoyTennenbaum <roytenen10@gmail.com>
2025-07-27 19:50:15 +0300

    word types c90 compliance

--- Headers/word-types.h
+++ Headers/word-types.h
@@ -1,48 +1,42 @@
 #ifndef WORD_TYPES_H
 #define WORD_TYPES_H
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short dest_op_bits : 2;
-    unsigned short src_op_bits : 2;
-    unsigned short opcode_bits : 4;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int dest_op_bits : 2;
+    unsigned int src_op_bits : 2;
+    unsigned int opcode_bits : 4;
 } cmdFirstWord;
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short value_bits : 8;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int value_bits : 8;
 } immediateAddressingWord;
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short value_bits : 8;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int value_bits : 8;
 } directAddressingWord;
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short label_address_bits : 8;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int label_address_bits : 8;
 } matrixAddressingFirstWord;
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short col_reg_bits : 3;
-    :1;
-    unsigned short row_reg_bits : 3;
-    :1;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int col_reg_bits : 3;
+    unsigned int unused_padding_1 : 1;
+    unsigned int row_reg_bits : 3;
+    unsigned int unused_padding_2 : 1;
 } matrixAddressingSecondWord;
 
-typedef struct
-{
-    unsigned short aer_bits : 2;
-    unsigned short dest_reg_bits : 3;
-    :1;
-    unsigned short src_reg_bits : 3;
-    :1;
+typedef struct {
+    unsigned int aer_bits : 2;
+    unsigned int dest_reg_bits : 3;
+    unsigned int unused_padding_1 : 1;
+    unsigned int src_reg_bits : 3;
+    unsigned int unused_padding_2 : 1;
 } registerAddressingWord;
 
 #endif
